ddde64c8ada1eefb76129fcee5226b8e1cae743c
Lars Fredrik Lunde <fredrilu@gmail.com>
2013-03-08 22:55:27 +0100

    Hello world

---
+++ HelloWorldUserControl.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="HelloWorldUserControl.ascx.cs" Inherits="HelloWorldUserControl" %>
+<asp:Label ID="Label1" runat="server" Text="blank"></asp:Label>
+<br />
+<br />
+<asp:Button ID="Button1" runat="server" Text="Click me!" 
+    onclick="Button1_Click" />
---
+++ HelloWorldUserControl.ascx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HelloWorldUserControl : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        UserName = Environment.UserName;
+        Label1.Text = UserName;
+    }
+
+    public string UserName { get; set; }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "Button clicked!";
+        Thread.Sleep(2000);
+        Label1.Text = UserName;
+    }
+}
